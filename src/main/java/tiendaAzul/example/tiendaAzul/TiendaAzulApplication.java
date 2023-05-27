db4e9d524b13482d20019db109ee8cb31cb834fc
Jorge Daniel Silva <daniel1jorge@hotmail.com>
2023-05-26 21:05:32 -0300

    commit inicial de proyecto.

---
+++ src/main/java/tiendaAzul/example/tiendaAzul/TiendaAzulApplication.java
@@ -0,0 +1,13 @@
+package tiendaAzul.example.tiendaAzul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAzulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tiendaAzul/example/tiendaAzul/TiendaAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package tiendaAzul.example.tiendaAzul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
